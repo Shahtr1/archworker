233712ea217a1362ea8d0e0abcbfc1e244f1c6c0
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2024-05-01 00:27:00 +0530

    Added JwtUtilUnitTests

--- Core Application/backend/src/test/java/com/archworker/coreapplication/unit/util/security/JwtUtilUnitTests.java
+++ Core Application/backend/src/test/java/com/archworker/coreapplication/unit/util/security/JwtUtilUnitTests.java
@@ -1,6 +1,13 @@
 package com.archworker.coreapplication.unit.util.security;
 
 import com.archworker.coreapplication.util.security.JwtUtil;
+import com.archworker.coreapplication.util.security.SecurityConstants;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +17,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collections;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -51,15 +59,40 @@ public class JwtUtilUnitTests {
     @DisplayName("Expired token should not be valid")
     void whenTokenIsExpired_thenShouldNotValidate() {
         // Arrange
-        // Act
-        // Assert
+        String username = "user";
+        UserDetails userDetails = new User(username, "password", Collections.emptyList());
+        String token = jwtUtil.generateToken(username);
+
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(
+                        Keys.hmacShaKeyFor(
+                                Decoders.BASE64.decode(SecurityConstants.TOKEN_SECRET)))
+                .build().parseClaimsJws(token).getBody();
+
+        // Set to past date
+        claims.setExpiration(new Date(System.currentTimeMillis() - 1000));
+
+        String expiredToken = Jwts.builder().setClaims(claims).compact();
+
+        // Act & Assert
+        assertThrows(ExpiredJwtException.class,
+                ()->jwtUtil.validateToken(expiredToken, userDetails));
     }
 
     @Test
     @DisplayName("Token with invalid signature should not validate")
     void whenTokenSignatureIsInvalid_thenShouldNotValidate() {
         // Arrange
-        // Act
-        // Assert
+        String username = "user";
+        UserDetails userDetails = new User(username, "password", Collections.emptyList());
+        String token = jwtUtil.generateToken(username);
+
+        // Tamper with the token to make the signature invalid
+        String invalidToken = token.substring(0, token.lastIndexOf(".") + 1);
+
+        // Act & Assert
+        assertThrows(JwtException.class, () -> jwtUtil.validateToken(invalidToken, userDetails),
+                "Invalid signature should cause JwtException");
+
     }
 }

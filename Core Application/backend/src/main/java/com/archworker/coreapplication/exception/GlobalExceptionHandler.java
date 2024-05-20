e9a0261b404a6b1e825a67eabc983e7145e76acf
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2024-05-20 22:16:20 +0530

    Added global exception case

--- Core Application/backend/src/main/java/com/archworker/coreapplication/exception/GlobalExceptionHandler.java
+++ Core Application/backend/src/main/java/com/archworker/coreapplication/exception/GlobalExceptionHandler.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,7 +17,7 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
     private final String validationErrorName = "Validation Error";
 
     @ExceptionHandler(ConstraintViolationException.class)
@@ -58,6 +57,19 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         );
 
         return new ResponseEntity<>(errorDTO, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDTO> handleGlobalException
+            (Exception exception, WebRequest request) {
+        ErrorDTO errorDTO = new ErrorDTO(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                validationErrorName,
+                List.of(exception.getMessage()),
+                request.getDescription(false)
+        );
+        return new ResponseEntity<>(errorDTO, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }

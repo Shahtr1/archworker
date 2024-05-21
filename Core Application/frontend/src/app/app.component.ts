c28f1eeeb19df2ae92ac2fce99b5be313a73d55b
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2024-05-21 13:36:54 +0530

    Started working on generate app demo

--- Core Application/frontend/src/app/app.component.ts
+++ Core Application/frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { GenerateAppComponent } from './generate-app/generate-app.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'arch-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, GenerateAppComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })

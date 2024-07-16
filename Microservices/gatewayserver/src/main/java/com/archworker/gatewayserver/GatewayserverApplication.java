79292fc050528eabbb5125f25a259007a770e4b4
Shahtr1 <tramboos123@gmail.com>
2024-07-16 21:24:05 +0530

    Implemented Custom routing

--- Microservices/gatewayserver/src/main/java/com/archworker/gatewayserver/GatewayserverApplication.java
+++ Microservices/gatewayserver/src/main/java/com/archworker/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,32 @@ package com.archworker.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator archworkerRouteConfig(RouteLocatorBuilder builder) {
+		String[] generators = {"angulargenerator"};
+
+		RouteLocatorBuilder.Builder routes = builder.routes();
+
+		for (String generator : generators) {
+			String path = "/arch/" + generator;
+			routes.route(generator,
+					p -> p.path(path + "/**")
+							.filters(f -> f.rewritePath(path + "/(?<segment>.*)", "/${segment}"))
+							.uri("lb://" + generator.toUpperCase()));
+		}
+
+		return routes.build();
+	}
 }

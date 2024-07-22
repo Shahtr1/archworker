2aec31d475acb756ae76b89b0f3757dcc5bf5914
Shahrukh <tramboos123@gmail.com>
2024-07-22 06:41:37 +0530

    testing workflow for microservices

--- Microservices/eurekaserver/src/test/java/com/archworker/eurekaserver/EurekaserverApplicationTests.java
+++ Microservices/eurekaserver/src/test/java/com/archworker/eurekaserver/EurekaserverApplicationTests.java
@@ -3,11 +3,14 @@ package com.archworker.eurekaserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class EurekaserverApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        fail();
+    }
 
 }

10fface63111c37b4dadd073090bc5a0bb7134cc
Shahrukh <tramboos123@gmail.com>
2024-08-02 08:17:40 +0530

    Added rate limiter pattern in gateway server

---
+++ Microservices/gatewayserver/src/main/java/com/archworker/gatewayserver/configuration/GatewayConfig.java
@@ -0,0 +1,15 @@
+package com.archworker.gatewayserver.configuration;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    KeyResolver userKeyResolver() {
+        return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("user"))
+                .defaultIfEmpty("anonymous");
+    }
+}

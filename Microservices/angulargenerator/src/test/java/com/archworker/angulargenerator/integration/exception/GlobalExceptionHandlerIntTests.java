a455f1f71472bb76b88823410dfdbe1f75d83214
Shahrukh <tramboos123@gmail.com>
2024-08-02 16:10:29 +0530

    moved to yaml completely

--- Microservices/angulargenerator/src/test/java/com/archworker/angulargenerator/integration/exception/GlobalExceptionHandlerIntTests.java
+++ Microservices/angulargenerator/src/test/java/com/archworker/angulargenerator/integration/exception/GlobalExceptionHandlerIntTests.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestPropertySource(locations = "/application-test.properties")
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 public class GlobalExceptionHandlerIntTests {
     @Autowired

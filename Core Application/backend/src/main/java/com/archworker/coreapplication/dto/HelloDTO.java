5d32f2f9d66666674b9bace69a946e4cb198ab28
Shahtr1 <tramboos123@gmail.com>
2024-10-06 16:17:22 +0530

    Renamed backend to api and Core Application to Portal

--- Core Application/backend/src/main/java/com/archworker/coreapplication/dto/HelloDTO.java
+++
@@ -1,4 +0,0 @@
-package com.archworker.coreapplication.dto;
-
-public record HelloDTO(String message) {
-}
---
+++ Portal/api/src/main/java/com/archworker/api/dto/HelloDTO.java
@@ -0,0 +1,4 @@
+package com.archworker.api.dto;
+
+public record HelloDTO(String message) {
+}
